6f670236390cb0913a04ee98fe940590970c7142
Jelena-Milev <jeka.milev@gmail.com>
2020-07-25 20:00:23 +0200

    project initialized

---
+++ src/main/java/com/fon/is/fpis/byproductdisposal/ByproductDisposalApplication.java
@@ -0,0 +1,13 @@
+package com.fon.is.fpis.byproductdisposal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByproductDisposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByproductDisposalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fon/is/fpis/byproductdisposal/ByproductDisposalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fon.is.fpis.byproductdisposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByproductDisposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
