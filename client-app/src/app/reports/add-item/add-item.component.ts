111b7821aba8623d4b2882a096d5e0aa1e7b2e09
Jelena-Milev <jeka.milev@gmail.com>
2020-08-21 17:11:39 +0200

    client-app: add report page - add item form and table extracted in separate component

--- client-app/src/app/reports/add-item/add-item.component.ts
+++ client-app/src/app/reports/add-item/add-item.component.ts
@@ -1,36 +1,74 @@
-import { Component, OnInit, OnDestroy, AfterViewInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  OnDestroy,
+  AfterViewInit,
+  ViewChild,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ByproductService } from 'src/app/service/byproduct.service';
 import { Byproduct } from 'src/app/model/byproduct.model';
 import { Subscription } from 'rxjs';
+import { ReportItem } from 'src/app/model/reportItem.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-add-item',
   templateUrl: './add-item.component.html',
-  styleUrls: ['./add-item.component.css']
+  styleUrls: ['./add-item.component.css'],
 })
 export class AddItemComponent implements OnInit, OnDestroy, AfterViewInit {
-
   byproducts: Byproduct[] = [];
 
   itemForm: FormGroup = new FormGroup({
-    byproduct: new FormControl(),
-    quantityForDisposal: new FormControl(),
+    byproduct: new FormControl(null, Validators.required),
+    quantityForDisposal: new FormControl('', [Validators.required, Validators.min(0.001)]),
   });
 
   private byproductSub: Subscription;
 
-  constructor(
-    private byproductService: ByproductService
-  ) { }
+  dataSource = new MatTableDataSource<ReportItem>();
+  displayedColumns: string[] = [
+    'id',
+    'byproductName',
+    'measurementUnit',
+    'quantityForDisposal',
+    'delete',
+  ];
+  // private paginator: MatPaginator;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+
+  items: ReportItem[] = [];
+  @Output() onItemsChanged: EventEmitter<ReportItem[]> = new EventEmitter<
+    ReportItem[]
+  >();
+
+  constructor(private byproductService: ByproductService) {}
+
+  // @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
+  //   this.paginator = mp;
+  //   this.setDataSourceAttributes();
+  // }
+
+  // setDataSourceAttributes() {
+  //   this.dataSource.paginator = this.paginator;
+  // }
 
   ngOnInit(): void {
-    this.byproductSub = this.byproductService.fetchByproducts().subscribe((res) => {
-          this.byproducts = res;
-        });
+    this.dataSource.data = this.items;
+    this.byproductSub = this.byproductService
+      .fetchByproducts()
+      .subscribe((res) => {
+        this.byproducts = res;
+      });
   }
 
   ngAfterViewInit(): void {
+    // this.dataSource.data = this.items;
+    this.dataSource.paginator = this.paginator;
     this.byproductService.fetchByproducts().subscribe();
   }
   ngOnDestroy(): void {
@@ -38,25 +76,34 @@ export class AddItemComponent implements OnInit, OnDestroy, AfterViewInit {
   }
 
   onAddItem() {
-  //   const byproduct = this.itemForm.get('byproduct').value;
-  //   const quantityForDisposal = this.itemForm.get('quantityForDisposal').value;
-  //   const itemIndex: number = this.selectedReport.items.findIndex(
-  //     (item) => item.byproduct.name === byproduct.name
-  //   );
-  //   if (itemIndex === -1) {
-  //     const newItem: ReportItem = new ReportItem(
-  //       byproduct,
-  //       quantityForDisposal
-  //     );
-  //     this.selectedReport.items.push(newItem);
-  //   } else {
-  //     this.selectedReport.items[
-  //       itemIndex
-  //     ].quantityForDisposal += quantityForDisposal;
-  //   }
-  //   this.dataSource.data = this.selectedReport.items;
-  //   this.itemForm.reset();
-  //   this.itemForm.markAsPristine();
-  //   this.itemForm.markAllAsTouched();
+    console.log('on add item');
+    console.log(this.itemForm);
+    const byproduct = this.itemForm.get('byproduct').value;
+    console.log(this.itemForm.get('byproduct').value)
+    const quantityForDisposal = this.itemForm.get('quantityForDisposal').value;
+    console.log(this.itemForm.get('quantityForDisposal').value)
+    const itemIndex: number = this.items.findIndex(
+      (item) => item.byproduct.name === byproduct.name
+    );
+    if (itemIndex === -1) {
+      const newItem: ReportItem = new ReportItem(
+        byproduct,
+        quantityForDisposal
+      );
+      this.items.push(newItem);
+    } else {
+      this.items[itemIndex].quantityForDisposal += quantityForDisposal;
+    }
+    this.dataSource.data = this.items;
+    this.onItemsChanged.emit(this.items);
+  }
+
+  onDeleteItem(element: ReportItem) {
+    const itemIndex = this.items.findIndex(
+      (item) => item.byproduct.name === element.byproduct.name
+    );
+    this.items.splice(itemIndex, 1);
+    this.dataSource.data = this.items;
+    this.onItemsChanged.emit(this.items);
   }
 }
--- client-app/src/app/reports/add-report/add-report.component.ts
+++ client-app/src/app/reports/add-report/add-report.component.ts
@@ -2,21 +2,16 @@ import {
   Component,
   OnInit,
   OnDestroy,
-  ViewChild,
   AfterViewInit,
 } from '@angular/core';
 import { WarehouseService } from '../../service/warehouse.service';
 import { EmployeeService } from '../../service/employee.service';
 import { ReportService } from '../../service/report.service';
-import { ByproductService } from '../../service/byproduct.service';
 import { Warehouse } from '../../model/warehouse.model';
 import { Employee } from '../../model/employee.model';
 import { ReportItem } from '../../model/reportItem.model';
-import { Byproduct } from '../../model/byproduct.model';
 import { Subscription } from 'rxjs';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { ErrorDialogComponent } from 'src/app/error-dialog/error-dialog.component';
 import { MatDialog } from '@angular/material/dialog';
@@ -43,48 +38,21 @@ export class AddReportComponent implements OnInit, OnDestroy, AfterViewInit {
     employee: new FormControl(null, Validators.required),
   });
 
-  itemForm: FormGroup = new FormGroup({
-    byproduct: new FormControl(null, Validators.required),
-    quantity: new FormControl('', [Validators.required, Validators.min(0.001)]),
-  });
-
   warehouses: Warehouse[] = [];
   employees: Employee[] = [];
-  byproducts: Byproduct[] = [];
 
   private warehouseSub: Subscription;
   private employeeSub: Subscription;
-  private byproductsSub: Subscription;
 
   items: ReportItem[] = [];
-
-  dataSource = new MatTableDataSource<ReportItem>();
-  displayedColumns: string[] = [
-    'id',
-    'byproductName',
-    'measurementUnit',
-    'quantityForDisposal',
-    'delete',
-  ];
-  private paginator: MatPaginator;
 
   constructor(
     private dialog: MatDialog,
     private snackBar: MatSnackBar,
     private warehouseService: WarehouseService,
     private employeeService: EmployeeService,
-    private byproductService: ByproductService,
     private reportService: ReportService
   ) {}
-
-  @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
-    this.paginator = mp;
-    this.setDataSourceAttributes();
-  }
-
-  setDataSourceAttributes() {
-    this.dataSource.paginator = this.paginator;
-  }
 
   ngOnInit(): void {
     this.warehouseSub = this.warehouseService
@@ -98,17 +66,9 @@ export class AddReportComponent implements OnInit, OnDestroy, AfterViewInit {
       .subscribe((res) => {
         this.employees = res;
       });
-
-    this.byproductsSub = this.byproductService
-      .fetchByproducts()
-      .subscribe((res) => {
-        this.byproducts = res;
-      });
   }
 
   ngAfterViewInit(): void {
-    this.dataSource.data = this.items;
-    this.dataSource.paginator = this.paginator;
     this.employeeService.fetchEmployees().subscribe();
     this.warehouseService.fetchWarehouses().subscribe();
   }
@@ -116,34 +76,10 @@ export class AddReportComponent implements OnInit, OnDestroy, AfterViewInit {
   ngOnDestroy(): void {
     this.warehouseSub.unsubscribe();
     this.employeeSub.unsubscribe();
-    this.byproductsSub.unsubscribe();
   }
 
-  onAddItem() {
-    console.log(this.itemForm);
-    const byproduct = this.itemForm.get('byproduct').value;
-    const quantityForDisposal = this.itemForm.get('quantity').value;
-    const itemIndex: number = this.items.findIndex(
-      (item) => item.byproduct.name === byproduct.name
-    );
-    if (itemIndex === -1) {
-      const newItem: ReportItem = new ReportItem(
-        byproduct,
-        quantityForDisposal
-      );
-      this.items.push(newItem);
-    } else {
-      this.items[itemIndex].quantityForDisposal += quantityForDisposal;
-    }
-    this.dataSource.data = this.items;
-  }
-
-  onDeleteItem(element: ReportItem) {
-    const itemIndex = this.items.findIndex(
-      (item) => item.byproduct.name === element.byproduct.name
-    );
-    this.items.splice(itemIndex, 1);
-    this.dataSource.data = this.items;
+  itemsChanged(items: ReportItem[]){
+    this.items = items;
   }
 
   onSaveReport() {
@@ -169,7 +105,6 @@ export class AddReportComponent implements OnInit, OnDestroy, AfterViewInit {
         this.reportForm.get('note').setValue(report.note);
         this.reportForm.get('warehouse').setValue(report.warehouse.id);
         this.reportForm.get('employee').setValue(report.employee.id);
-        this.dataSource.data = report.items;
         this.snackBar.open('Uspešno sačuvan izveštaj', '', {
           duration: 2000,
         });
